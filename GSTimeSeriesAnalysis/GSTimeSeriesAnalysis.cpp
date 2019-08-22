17a0cd0c2f8564c22582ace6bbad9e09b0fccd67
rroper <rroper@goldsim.com>
2019-08-21 19:33:42 -0700

    Added new project with TimeSeries class that can be used to store and analyze time series (for now, calculate mean and standard deviation and calculate correlations between two time series); Added a DLL project that uses the TimeSeries class and makes it possible for such calculations to be done in GoldSim.

---
+++ GSTimeSeriesAnalysis/GSTimeSeriesAnalysis.cpp
@@ -0,0 +1,234 @@
+
+#include "pch.h"
+#include "TimeSeries.h"
+
+//  Global enumerations, useful for C-style implementations 
+
+//  Every external function is called by GoldSim with specific requests including:
+//    - initialization
+//    - performing calculations
+//    - obtaining the version number
+//    - obtaining the number of input and output arguments
+//    - "cleaning up" after the simulation (e.g. releasing memory and closing files)
+//
+//  XFMethodID (below) identifies the method types used to identify the phase of the simulation that is currently in progress.
+//
+//    XF_INITIALIZE    - Called after DLL is loaded and before each realization (no arguments are passed on this call). 
+//    XF_CALCULATE     - Called during the simulation, each time the inputs change. 
+//    XF_REP_VERSION   - Called after DLL load to report the external fcn version number. 
+//    XF_REP_ARGUMENTS - Called after DLL load to report the number of input and output arguments. 
+//    XF_CLEANUP       - Called before the DLL is unloaded. 
+
+enum XFMethodID
+{
+	XF_INITIALIZE = 0,
+	XF_CALCULATE = 1,
+	XF_REP_VERSION = 2,
+	XF_REP_ARGUMENTS = 3,
+	XF_CLEANUP = 99
+};
+
+// Declare global variables that persist between calls from GoldSim to the DLL
+double time_index;		// Use this in functions where time values do not matter
+TimeSeries* ts1;
+TimeSeries* ts2;
+int initialized = -1;	// This should be set to a value >= 0 on the first call to the DLL
+
+//  XFStatusID (below) identifies the return codes for external functions. 
+//      
+//    XF_SUCCESS          – Call completed successfully. 
+//    XF_CLEANUP_NOW      - Call was successful, but GoldSim should clean up and unload the DLL immediately. 
+//    XF_FAILURE          - Failure (no error information returned).  
+//    XF_FAILURE_WITH_MSG – Failure, with DLL-supplied error message available. Address of error message is returned in the first element of the output arguments array. 
+//    XF_INCREASE_MEMORY  - Failed because the memory allocated for output arguments is too small.  GoldSim will increase the size of the output argument array and try again. 
+
+// Return codes:
+//   0 indicates OK, continue GoldSim
+//   >0 and <99 indicates to terminate GoldSim
+//   99 indicates OK, unload the DLL
+//
+// The following codes can only be used for XF_CALCULATE:
+//   -1 indicates fatal error and an error message pointer is returned
+//   -2 indicates more result memory is required; total amount in doubles is returned in outargs[0]
+enum XFStatusID
+{
+	XF_SUCCESS = 0,
+	XF_FAILURE = 1,
+	XF_CLEANUP_NOW = 99,
+	XF_FAILURE_WITH_MSG = -1,
+	XF_INCREASE_MEMORY = -2
+};
+
+//  When calling methods from a DLL, GoldSim always expects the following C/C++ function signature:
+//     extern "C" void __declspec(dllexport) MyExternalFcn(int XFMethod, int* XFState, double* inargs, double* outargs)
+//
+//  Notes:
+//    - The function name and argument names can be different from those shown above
+//    - "C" specifies C language style linkage between GoldSim and the DLL
+//    - __declspec(dllexport) makes the function visible outside the DLL
+//    - XFMethod: specifies the action that the external function must perform
+//    - XFState: a code indicating the status of the external function
+//    - inargs: array of input arguments
+//    - outargs: this array returns different information for different XFMethod values
+
+// Calculate basic statistics for values in a time series
+//-----------------------------------------------------------------------------------------------
+extern "C" void __declspec(dllexport) GetTimeSeriesStatistics(int methodID, int* status, double* inargs, double* outargs)
+{
+	*status = XF_SUCCESS;
+	//double current_x;
+
+	switch (methodID)
+	{
+		// Initialize (called at beginning of each realization)
+		// For example, allocate memory or open files
+		// No arguments are passed on this call
+	case  XF_INITIALIZE:
+		// Initialize global variables
+		break;	// no initialization in this example
+
+	// The external function reports its version
+	// No input arguments are passed on this call.
+	// outargs[0] is set equal to the external fcn version
+	case  XF_REP_VERSION:
+		outargs[0] = 1.01;
+		break;
+
+		// External fcn reports the number of input and output arguments
+		// outargs[0] is set equal to the # of inputs arguments
+		// outargs[1] is set equal to the # of output arguments
+	case  XF_REP_ARGUMENTS:
+		// A single value from GoldSim is expected on each call to this function
+		outargs[0] = 1.0;
+
+		// For now, there are two return values, the mean and standard deviation of the time series
+		outargs[1] = 2.0;
+		break;
+
+		// Normal calculation.
+		// Results are returned as outarg[0], outarg[1], etc. depending on number of outputs
+	case  XF_CALCULATE:
+		// Initialize global variables
+		if (initialized < 0)
+		{
+			// Initialize time index
+			time_index = 0.0;
+
+			// Set 'initialized' >= 0 so that this block is only executed on the first call to the DLL
+			initialized = 1;
+
+			// Initialize the time series
+			ts1 = new TimeSeries();
+		}
+
+		// Add a new time point to the time series
+		ts1->addTimepoint(time_index, inargs[0]);
+
+		// Calculate and return the mean and standard deviation of the stored time series values
+		outargs[0] = ts1->mean();
+		outargs[1] = ts1->stdev();
+
+		// Increment the time series index
+		time_index += 1.0;
+
+		break;
+
+		// Close any open files
+		// Optionally release any memory that's been allocated
+		// No arguments are passed on this call.
+	case  XF_CLEANUP:
+		// Free memory allocated for time series object(s)
+		delete ts1;
+		ts1 = 0;
+		break;
+
+		// Error if this point is reached
+		// This means the switch statement did not provide the cases that GoldSim expected.
+		// The external function must have cases 0, 1, 2, 3 and 99
+	default:
+		*status = XF_FAILURE;
+		break;
+	}
+}
+
+// Calculate the correlation between two time series
+//-----------------------------------------------------------------------------------------------
+extern "C" void __declspec(dllexport) GetTimeSeriesCorrelation(int methodID, int* status, double* inargs, double* outargs)
+{
+	*status = XF_SUCCESS;
+	//double current_x;
+
+	switch (methodID)
+	{
+		// Initialize (called at beginning of each realization)
+		// For example, allocate memory or open files
+		// No arguments are passed on this call
+	case  XF_INITIALIZE:
+		// Initialize global variables
+		break;	// no initialization in this example
+
+	// The external function reports its version
+	// No input arguments are passed on this call.
+	// outargs[0] is set equal to the external fcn version
+	case  XF_REP_VERSION:
+		outargs[0] = 1.01;
+		break;
+
+		// External fcn reports the number of input and output arguments
+		// outargs[0] is set equal to the # of inputs arguments
+		// outargs[1] is set equal to the # of output arguments
+	case  XF_REP_ARGUMENTS:
+		// Two value from GoldSim are expected on each call to this function
+		outargs[0] = 2.0;
+
+		// Return the value of the correlation between the two time series
+		outargs[1] = 1.0;
+		break;
+
+		// Normal calculation.
+		// Results are returned as outarg[0], outarg[1], etc. depending on number of outputs
+	case  XF_CALCULATE:
+		// Initialize global variables
+		if (initialized < 0)
+		{
+			// Initialize time index
+			time_index = 0.0;
+
+			// Set 'initialized' >= 0 so that this block is only executed on the first call to the DLL
+			initialized = 1;
+
+			// Initialize the two time series
+			ts1 = new TimeSeries();
+			ts2 = new TimeSeries();
+		}
+
+		// Add new time points to the two time series
+		ts1->addTimepoint(time_index, inargs[0]);
+		ts2->addTimepoint(time_index, inargs[1]);
+
+		// Calculate and return the correlation of values in the two time series
+		outargs[0] = ts1->correlation(*ts2);
+
+		// Increment the time series index
+		time_index += 1.0;
+
+		break;
+
+		// Close any open files
+		// Optionally release any memory that's been allocated
+		// No arguments are passed on this call.
+	case  XF_CLEANUP:
+		// Free memory allocated for time series object(s)
+		delete ts1;
+		delete ts2;
+		ts1 = ts2 = 0;
+		break;
+
+		// Error if this point is reached
+		// This means the switch statement did not provide the cases that GoldSim expected.
+		// The external function must have cases 0, 1, 2, 3 and 99
+	default:
+		*status = XF_FAILURE;
+		break;
+	}
+}
---
+++ TimeSeries/TimeSeries.cpp
@@ -0,0 +1,134 @@
+#include "pch.h"
+#include "TimeSeries.h"
+
+// Default constructor
+TimeSeries::TimeSeries()
+{
+}
+
+// Add a new time point and value to the time series
+bool TimeSeries::addTimepoint(double time, double value)
+{
+	// Push new time and value
+	times.push_back(time);
+	values.push_back(value);
+
+	// Return true if this point is reached
+	// (For now, always assume success)
+	return true;
+}
+
+// Get value at the specified index location (regardless of time)
+double TimeSeries::getValueByIndex(int i)
+{
+	// Check that the index is in the valid range
+	if (i >= 0 && size_t(i) < values.size())
+	{
+		return values[i];
+	}
+	// Otherwise, return a zero value
+	else
+	{
+		return 0.0;
+	}
+}
+
+// Get the size of the time series
+int TimeSeries::size()
+{
+	// Assume that size of time and value vectors are equal
+	// Note: addTimepoint function doesn't allow these to be unequal
+	return values.size();
+}
+
+// Mean of all values in the time series
+double TimeSeries::mean()
+{
+	return mean(values.size());
+}
+
+// Get the mean of a maximum of the first n values
+double TimeSeries::mean(int n)
+{
+	double sum = 0.0;
+
+	// Number of values to use to calculate the mean
+	int number_of_values_to_use = min(values.size(), size_t(n));
+
+	if ( number_of_values_to_use > 0 )
+	{
+		// Calculate sum of values and return the mean
+		for (int i = 0; i < number_of_values_to_use; i++) sum += values[i];
+		return sum / values.size();
+	}
+	else
+	{
+		// Return 0 if there is no data
+		return 0.0;
+	}
+}
+
+// Standard deviation of all values in the time series
+double TimeSeries::stdev()
+{
+	return stdev(values.size());
+}
+
+// Get the standard deviation of a maximum of the first n values
+double TimeSeries::stdev(int n)
+{
+	// Sum of squared deviations
+	double sum_of_squared_deviations = 0.0;
+
+	// Number of values to use to calculate the standard deviation
+	int number_of_values_to_use = min(values.size(), size_t(n));
+
+	if ( number_of_values_to_use > 0 )
+	{
+		// Get mean of values
+		double mean = TimeSeries::mean(number_of_values_to_use);
+
+		// Calculate sum of squared deviations from mean and return standard deviation
+		for (int i = 0; i < number_of_values_to_use; i++) sum_of_squared_deviations += pow(values[i] - mean, 2.0);
+		return sqrt(sum_of_squared_deviations / number_of_values_to_use);
+	}
+	else
+	{
+		// Return 0 if there is no data
+		return 0.0;
+	}
+}
+
+// Get correlation of values in 'this' TimeSeries with respect to TimeSeries ts
+double TimeSeries::correlation(TimeSeries& ts)
+{
+	// Sum of the product of the normalized deviations from means
+	double sum_product_deviations = 0.0;
+
+	// Get the maximum number of values that can be used in calculating the correlation
+	int number_of_values = min(TimeSeries::size(), ts.size());
+
+	// At least two values are required to calculate correlation
+	if ( number_of_values >= 2 )
+	{
+		// Get the means of the two time series
+		double mean1 = mean(number_of_values);
+		double mean2 = ts.mean(number_of_values);
+
+		// Get the standard deviations of the two time series
+		double stdev1 = stdev(number_of_values);
+		double stdev2 = ts.stdev(number_of_values);
+
+		// Calculate the sum of the products of normalized deviations from means and return correlation
+		for (int i = 0; i < number_of_values; i++)
+		{
+			sum_product_deviations += ((values[i] - mean1) / stdev1 * (ts.getValueByIndex(i) - mean2) / stdev2);
+		}
+		return sum_product_deviations / (number_of_values);// -1.0);	// Without the -1, results are identical to Excel
+	}
+	else
+	{
+		// Return 0 (no correlation) if there are insufficient values
+		return 0.0;
+	}
+}
